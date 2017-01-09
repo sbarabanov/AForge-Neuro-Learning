ad575a20ce1e3f4f2949ae579610690db08c53ac
Sergey Barabanov <barabanov.mail@gmail.com>
2017-01-09 11:33:35 +0300

    test neuro learning

--- AForge`Neuro`Learning/Program.cs
+++ AForge`Neuro`Learning/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using AForge.Neuro;
+using AForge.Neuro.Learning;
 
 namespace AForge_Neuro_Learning
 {
@@ -10,7 +8,55 @@ namespace AForge_Neuro_Learning
     {
         static void Main()
         {
+            // Создаем сеть с сигмодиной активацонной функцией и 4 слоями (с 10,3,3,10 нейронами в каждом слое, соответственно)
+            ActivationNetwork net = new ActivationNetwork(new SigmoidFunction(), 10, 3, 3, 10);
+            // Обучение - алгоритм с обратным распространением ошибки
+            BackPropagationLearning trainer = new BackPropagationLearning(net);
 
+            // Формируем множество входных векторов
+            double[][] input = new double[][] {
+                new double[]{1,1,1,1,1,1,1,1,1,1},
+                new double[]{0,0,0,0,0,0,0,0,0,0},
+                new double[]{1,1,1,1,1,0,0,0,0,0},
+                new double[]{0,0,0,0,0,1,1,1,1,1},
+            };
+
+            // Формируем множество желаемых выходных векторов
+            double[][] output = new double[][] {
+                new double[]{1,1,1,1,1,1,1,1,1,1},
+                new double[]{0,0,0,0,0,0,0,0,0,0},
+                new double[]{1,1,1,1,1,0,0,0,0,0},
+                new double[]{0,0,0,0,0,1,1,1,1,1},
+            };
+
+            // Переменная, сохраняющая значение ошибки сети на предыдущем шаге
+            double prErr = 10000000;
+            // Ошибка сети
+            double error = 100;
+            // Сначала скорость обучения должна быть высока
+            trainer.LearningRate = 1;
+            // Обучаем сеть пока ошибка сети станет небольшой
+            while (error > 0.001)
+            {
+                // Получаем ошибку сети
+                error = trainer.RunEpoch(input, output);
+                // Если ошибка сети изменилась на небольшое значения, в сравнении ошибкой предыдущей эпохи
+                if (Math.Abs(error - prErr) < 0.000000001)
+                {
+                    // Уменьшаем коэффициент скорости обучения на 2
+                    trainer.LearningRate /= 2;
+                    if (trainer.LearningRate < 0.001)
+                        trainer.LearningRate = 0.001;
+                }
+
+                prErr = error;
+            }
+
+            double[] result;
+            result = net.Compute(new double[] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 });
+            result = net.Compute(new double[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 });
+            result = net.Compute(new double[] { 1, 1, 1, 1, 1, 0, 0, 0, 0, 0 });
+            result = net.Compute(new double[] { 0, 0, 0, 0, 0, 1, 1, 1, 1, 1 });
         }
     }
 }
