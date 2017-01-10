137e0665358f11093d6917147c1f6b0d776a9bb1
Sergey Barabanov <barabanov.mail@gmail.com>
2017-01-10 14:35:01 +0300

    first result learning

--- AForge`Neuro`Learning/Program.cs
+++ AForge`Neuro`Learning/Program.cs
@@ -16,61 +16,135 @@ namespace AForge_Neuro_Learning
             Dictionary<DateTime, string> scores = GetScores("NBA`10`11");
 
             string[] opps = scores.SelectMany(sc => new string[] { sc.Value.Split('#')[0].Trim(), sc.Value.Split('#')[1].Trim() }).Distinct().ToArray();
+            Dictionary<string, double[]> last = opps.ToDictionary(k => k, v => GetVector(v, scores.Select(sc => sc.Value).ToArray()));
 
-            Dictionary<string, double[]> last = opps.ToDictionary(
-                k => k,
-                v => new double[] {
-                    // число побед дома
-                    scores.Values.Where(sc => sc.Split('#')[0].Trim() == v).Count(sc => Convert.ToInt32(sc.Split('#')[2]) > Convert.ToInt32(sc.Split('#')[3])),
-                    // число побед в гостях
-                    scores.Values.Where(sc => sc.Split('#')[1].Trim() == v).Count(sc => Convert.ToInt32(sc.Split('#')[2]) < Convert.ToInt32(sc.Split('#')[3])),
-                    // число поражений дома
-                    scores.Values.Where(sc => sc.Split('#')[0].Trim() == v).Count(sc => Convert.ToInt32(sc.Split('#')[2]) < Convert.ToInt32(sc.Split('#')[3])),
-                    // число поражений в гостях
-                    scores.Values.Where(sc => sc.Split('#')[1].Trim() == v).Count(sc => Convert.ToInt32(sc.Split('#')[2]) > Convert.ToInt32(sc.Split('#')[3])),
-                    // число забитых мячей дома
-                    scores.Values.Where(sc => sc.Split('#')[0].Trim() == v).Sum(sc => Convert.ToInt32(sc.Split('#')[2])),
-                    // число забитых мячей в гостях
-                    scores.Values.Where(sc => sc.Split('#')[1].Trim() == v).Sum(sc => Convert.ToInt32(sc.Split('#')[3])),
-                    // число пропущенных мячей дома
-                    scores.Values.Where(sc => sc.Split('#')[0].Trim() == v).Sum(sc => Convert.ToInt32(sc.Split('#')[3])),
-                    // число пропущенных мячей в гостях
-                    scores.Values.Where(sc => sc.Split('#')[1].Trim() == v).Sum(sc => Convert.ToInt32(sc.Split('#')[2])),
-                });
+            // обучение
 
             List<List<double>> input = new List<List<double>> { };
             List<List<double>> output = new List<List<double>> { };
 
             scores = GetScores("NBA`11`12");
 
-            foreach (var pair in scores.OrderBy(pair => pair.Key))
+            foreach (var pair in scores.OrderBy(sc => sc.Key))
             {
-                if (! opps.Contains(pair.Value.Split('#')[0].Trim())) continue; 
+                if (! opps.Contains(pair.Value.Split('#')[0].Trim())) continue;
                 if (! opps.Contains(pair.Value.Split('#')[1].Trim())) continue;
 
                 input.Add(new List<double> { });
 
                 // данные за прошлый сезон команды дома
                 input.Last().AddRange(last[pair.Value.Split('#')[0].Trim()]);
+
+                // число дней с момента последней игры команды дома
+                DateTime frDate = scores
+                    .Where(sc => (new string[] { sc.Value.Split('#')[0].Trim(), sc.Value.Split('#')[1].Trim() }).Contains(pair.Value.Split('#')[0].Trim()))
+                    .Min(sc => sc.Key);
+
+                if (frDate == pair.Key)
+                    input.Last().Add(7);
+                else
+                {
+                    DateTime prDate = scores
+                        .Where(sc => (new string[] { sc.Value.Split('#')[0].Trim(), sc.Value.Split('#')[1].Trim() }).Contains(pair.Value.Split('#')[0].Trim()))
+                        .Where(sc => sc.Key < pair.Key).Max(sc => sc.Key);
+
+                    input.Last().Add((pair.Key - prDate).TotalDays);
+                }
+
                 // данные за прошлый сезон команды в гостях
                 input.Last().AddRange(last[pair.Value.Split('#')[1].Trim()]);
 
-                output.Add(new List<double> { });
+                // число дней с момента последней игры команды в гостях
+                frDate = scores
+                    .Where(sc => (new string[] { sc.Value.Split('#')[0].Trim(), sc.Value.Split('#')[1].Trim() }).Contains(pair.Value.Split('#')[1].Trim()))
+                    .Min(sc => sc.Key);
 
-                if (Convert.ToInt32(pair.Value.Split('#')[2]) > Convert.ToInt32(pair.Value.Split('#')[3]))
-                    output.Last().AddRange(new List<double> { 1, 0 });
+                if (frDate == pair.Key)
+                    input.Last().Add(7);
                 else
-                    output.Last().AddRange(new List<double> { 0, 1 });
+                {
+                    DateTime prDate = scores
+                        .Where(sc => (new string[] { sc.Value.Split('#')[0].Trim(), sc.Value.Split('#')[1].Trim() }).Contains(pair.Value.Split('#')[1].Trim()))
+                        .Where(sc => sc.Key < pair.Key).Max(sc => sc.Key);
+
+                    input.Last().Add((pair.Key - prDate).TotalDays);
+                }
+
+                output.Add(Convert.ToInt32(pair.Value.Split('#')[2]) > Convert.ToInt32(pair.Value.Split('#')[3]) ? new List<double> { 1, 0 } : new List<double> { 0, 1 });
             }
 
-            ActivationNetwork net = GetNetwork(input.Select(i => i.ToArray()).ToArray(), output.Select(o => o.ToArray()).ToArray());
+            ActivationNetwork net = GetNetwork(input.Select(i => i.Select(ii => ii / 1E6).ToArray()).ToArray(), output.Select(o => o.Select(oo => oo / 1E6).ToArray()).ToArray());
 
-            //double[] result = net.Compute(new double[] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 });
+            // анализ
+
+            opps = scores.SelectMany(sc => new string[] { sc.Value.Split('#')[0].Trim(), sc.Value.Split('#')[1].Trim() }).Distinct().ToArray();
+            last = opps.ToDictionary(k => k, v => GetVector(v, scores.Select(sc => sc.Value).ToArray()));
+
+            scores = GetScores("NBA`12`13");
+
+            int hit = 0;
+            int cnt = 0;
+
+            foreach (var pair in scores.OrderBy(sc => sc.Key))
+            {
+                if (! opps.Contains(pair.Value.Split('#')[0].Trim())) continue;
+                if (! opps.Contains(pair.Value.Split('#')[1].Trim())) continue;
+
+                List<double> list = new List<double> { };
+
+                // данные за прошлый сезон команды дома
+                list.AddRange(last[pair.Value.Split('#')[0].Trim()]);
+
+                // число дней с момента последней игры команды дома
+                DateTime frDate = scores
+                    .Where(sc => (new string[] { sc.Value.Split('#')[0].Trim(), sc.Value.Split('#')[1].Trim() }).Contains(pair.Value.Split('#')[0].Trim()))
+                    .Min(sc => sc.Key);
+
+                if (frDate == pair.Key)
+                    list.Add(7);
+                else
+                {
+                    DateTime prDate = scores
+                        .Where(sc => (new string[] { sc.Value.Split('#')[0].Trim(), sc.Value.Split('#')[1].Trim() }).Contains(pair.Value.Split('#')[0].Trim()))
+                        .Where(sc => sc.Key < pair.Key).Max(sc => sc.Key);
+
+                    list.Add((pair.Key - prDate).TotalDays);
+                }
+
+                // данные за прошлый сезон команды в гостях
+                list.AddRange(last[pair.Value.Split('#')[1].Trim()]);
+
+                // число дней с момента последней игры команды в гостях
+                frDate = scores
+                    .Where(sc => (new string[] { sc.Value.Split('#')[0].Trim(), sc.Value.Split('#')[1].Trim() }).Contains(pair.Value.Split('#')[1].Trim()))
+                    .Min(sc => sc.Key);
+
+                if (frDate == pair.Key)
+                    list.Add(7);
+                else
+                {
+                    DateTime prDate = scores
+                        .Where(sc => (new string[] { sc.Value.Split('#')[0].Trim(), sc.Value.Split('#')[1].Trim() }).Contains(pair.Value.Split('#')[1].Trim()))
+                        .Where(sc => sc.Key < pair.Key).Max(sc => sc.Key);
+
+                    list.Add((pair.Key - prDate).TotalDays);
+                }
+
+                double[] result = net.Compute(list.ToArray());
+
+                hit += (Convert.ToInt32(pair.Value.Split('#')[2]) > Convert.ToInt32(pair.Value.Split('#')[3]) && result[0] > result[1]) ? 1 : 0;
+                hit += (Convert.ToInt32(pair.Value.Split('#')[2]) < Convert.ToInt32(pair.Value.Split('#')[3]) && result[0] < result[1]) ? 1 : 0;
+
+                cnt++;
+            }
+
+            Console.WriteLine((double) hit / cnt);
+            Console.ReadLine();
         }
 
         static ActivationNetwork GetNetwork(double[][] input, double[][] output)
         {
-            ActivationNetwork net = new ActivationNetwork(new SigmoidFunction(), 16, 24, 32, 24, 16, 12, 8, 4, 2);
+            ActivationNetwork net = new ActivationNetwork(new SigmoidFunction(), 18, 12, 8, 4, 2);
             BackPropagationLearning trainer = new BackPropagationLearning(net);
 
             // Переменная, сохраняющая значение ошибки сети на предыдущем шаге
@@ -91,8 +165,8 @@ namespace AForge_Neuro_Learning
                     // Уменьшаем коэффициент скорости обучения на 2
                     trainer.LearningRate /= 2;
 
-                    if (trainer.LearningRate < 0.01)
-                        trainer.LearningRate = 0.01;
+                    if (trainer.LearningRate < 0.001)
+                        trainer.LearningRate = 0.001;
                 }
 
                 Console.WriteLine(error + " / " + trainer.LearningRate);
@@ -129,6 +203,30 @@ namespace AForge_Neuro_Learning
             }
 
             return dictionary;
+        }
+
+        static double[] GetVector(string opp, string[] scores)
+        {
+            double[] vector = new double[] {
+                // число побед дома
+                scores.Where(sc => sc.Split('#')[0].Trim() == opp).Count(sc => Convert.ToInt32(sc.Split('#')[2]) > Convert.ToInt32(sc.Split('#')[3])),
+                // число побед в гостях
+                scores.Where(sc => sc.Split('#')[1].Trim() == opp).Count(sc => Convert.ToInt32(sc.Split('#')[2]) < Convert.ToInt32(sc.Split('#')[3])),
+                // число поражений дома
+                scores.Where(sc => sc.Split('#')[0].Trim() == opp).Count(sc => Convert.ToInt32(sc.Split('#')[2]) < Convert.ToInt32(sc.Split('#')[3])),
+                // число поражений в гостях
+                scores.Where(sc => sc.Split('#')[1].Trim() == opp).Count(sc => Convert.ToInt32(sc.Split('#')[2]) > Convert.ToInt32(sc.Split('#')[3])),
+                // число забитых мячей дома
+                scores.Where(sc => sc.Split('#')[0].Trim() == opp).Sum(sc => Convert.ToInt32(sc.Split('#')[2])),
+                // число забитых мячей в гостях
+                scores.Where(sc => sc.Split('#')[1].Trim() == opp).Sum(sc => Convert.ToInt32(sc.Split('#')[3])),
+                // число пропущенных мячей дома
+                scores.Where(sc => sc.Split('#')[0].Trim() == opp).Sum(sc => Convert.ToInt32(sc.Split('#')[3])),
+                // число пропущенных мячей в гостях
+                scores.Where(sc => sc.Split('#')[1].Trim() == opp).Sum(sc => Convert.ToInt32(sc.Split('#')[2])),
+            };
+
+            return vector;
         }
     }
 }
